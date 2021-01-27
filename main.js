ce128c99a3d40aaaba9c12080c84a31721dce8ae
wyjeon <wyjeon.info@gmail.com>
2021-01-27 17:41:10 +0900

    18. 저자 수정 기능 구현

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var http = require("http");
 var url = require("url");
-var qs = require("querystring");
-var template = require("./lib/template.js");
-var db = require("./lib/db.js");
 var topic = require("./lib/topic");
 var author = require("./lib/author");
 
@@ -30,6 +27,10 @@ var app = http.createServer(function (request, response) {
     author.home(request, response);
   } else if (pathname === "/author/create_process") {
     author.create_process(request, response);
+  } else if (pathname === "/author/update") {
+    author.update(request, response);
+  } else if (pathname === "/author/update_process") {
+    author.update_process(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
