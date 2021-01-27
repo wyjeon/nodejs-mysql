abdde58a3046214931126d1d7f9a978fca41f254
wyjeon <wyjeon.info@gmail.com>
2021-01-27 15:28:29 +0900

    7.  MySQL로 글수정 기능 구현

--- main.js
+++ main.js
@@ -113,31 +113,39 @@ var app = http.createServer(function (request, response) {
       );
     });
   } else if (pathname === "/update") {
-    fs.readdir("./data", function (error, filelist) {
-      var filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-        var title = queryData.id;
-        var list = template.list(filelist);
-        var html = template.HTML(
-          title,
-          list,
-          `
+    db.query(`SELECT * FROM topic`, function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      db.query(
+        `SELECT * FROM topic WHERE id=?`,
+        [queryData.id],
+        function (error2, topic) {
+          if (error2) {
+            throw error2;
+          }
+          var list = template.list(topics);
+          var html = template.HTML(
+            topic[0].title,
+            list,
+            `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
               </p>
               <p>
                 <input type="submit">
               </p>
             </form>
             `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        }
+      );
     });
   } else if (pathname === "/update_process") {
     var body = "";
@@ -146,15 +154,14 @@ var app = http.createServer(function (request, response) {
     });
     request.on("end", function () {
       var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
+      db.query(
+        `UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`,
+        [post.title, post.description, post.id],
+        function (error, result) {
+          response.writeHead(302, { Location: `/?id=${post.id}` });
           response.end();
-        });
-      });
+        }
+      );
     });
   } else if (pathname === "/delete_process") {
     var body = "";
